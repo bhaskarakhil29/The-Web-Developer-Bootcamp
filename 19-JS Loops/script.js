acb9edf18a7aadd6e5beb8d87b3619386806bede
Idury Jnana Bhaskar Akhil <bhaskarakhil29@gmail.com>
2023-01-02 11:29:24 +0530

    Nested Loops

--- 19-JS Loops/script.js
+++ 19-JS Loops/script.js
@@ -2,6 +2,22 @@
 
 const animals = ['tigers','lions','bears'];
 
+//Common template
 for(let i=0;i<animals.length;i++){
     console.log(animals[i]);
+}
+
+
+//Nested Loops
+
+const seatingChart = [
+    ['Akhil','Bhaskar','Rocky'],
+    ['Vish','Narayan','Gurur','hruday'],
+    ['suman','venky','uday']
+]
+
+for(let i=0;i<seatingChart.length;i++){
+    for(let j=0;j<seatingChart[i].length;j++){
+        console.log(`Seating Chart is ${seatingChart[i][j]}`);
+    }
 }
